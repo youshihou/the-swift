76e75ba0b9ae418eefd1796274aa550955aa44e0
Ankui <youshihou5@gmail.com>
2021-12-11 13:17:33 +0800

    update code

--- sources/the-swift/the-swift/playground/S01E46.playground/Contents.swift
+++ sources/the-swift/the-swift/playground/S01E46.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+final class Header {
+    let view: UIView
+    let setTitle: (String) -> Void
+    
+    init(view: UIView, setTitle: @escaping (String) -> Void) {
+        self.view = view
+        self.setTitle = setTitle
+    }
+}
+
+class ParallaxView: UIView {
+    let headerView: Header
+    
+    init(frame: CGRect, headerView: Header) {
+        self.headerView = headerView
+        super.init(frame: frame)
+        
+        headerView.setTitle("Hello")
+        addSubview(headerView.view)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+let label = UILabel()
+let header = Header(view: label) { label.text = $0 }
+let p = ParallaxView(frame: .zero, headerView: header)
